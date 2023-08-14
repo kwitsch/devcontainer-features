e8e0f53f6c92f331a96d3317b32f9fd6a7ba8637
Kwitsch <kwitsch@gmail.com>
2023-08-14 01:08:10 +0000

    better tests

--- test/shared-package-cache/go-enabled.sh
+++ test/shared-package-cache/go-enabled.sh
@@ -5,6 +5,10 @@ set -e
 # shellcheck disable=SC1091
 source dev-container-features-test-lib
 
-check "test if go folder exists" find /tmp/package-cache/go -type d &>/dev/null
+go install golang.org/x/tools/cmd/goimports@latest
+
+ls -la /tmp/package-cache/go
+
+check "test if go folder contains files" sh -c 'find /tmp/package-cache/go -maxdepth 0 -type d &>/dev/null || find /tmp/package-cache/go -maxdepth 0 -type f &>/dev/null'
 
 reportResults
--- test/shared-package-cache/npm-enabled.sh
+++ test/shared-package-cache/npm-enabled.sh
@@ -5,6 +5,10 @@ set -e
 # shellcheck disable=SC1091
 source dev-container-features-test-lib
 
-check "test if npm folder exists" find /tmp/package-cache/npm -type d &>/dev/null
+npm install -g hello-world-npm
+
+ls -la /tmp/package-cache/npm
+
+check "test if npm folder contains files" sh -c 'find /tmp/package-cache/npm -maxdepth 0 -type d &>/dev/null || find /tmp/package-cache/npm -maxdepth 0 -type f &>/dev/null'
 
 reportResults
